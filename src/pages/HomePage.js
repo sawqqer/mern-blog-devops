ea5a2930ffa10ba89fb59a1259ffaec5d8a2fb5e
sawqqer <gcom0733@gmail.com>
2025-10-16 02:23:01 +0500

    Update homepage title - trigger CI/CD

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const HomePage = () => (
     <>
-    <h1>New blog coming soon!</h1>
+    <h1>🚀 MERN Blog with DevOps CI/CD!</h1>
     <p>
         Welcome to my blog. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Eget mi proin sed libero. Vestibulum mattis ullamcorper velit sed. Accumsan in nisl nisi scelerisque. Donec ac odio tempor orci. Sit amet mauris commodo quis imperdiet massa tincidunt. Nisl tincidunt eget nullam non nisi est. Volutpat diam ut venenatis tellus in metus vulputate. Leo vel fringilla est ullamcorper eget. Nunc pulvinar sapien et ligula ullamcorper malesuada. Viverra nibh cras pulvinar mattis nunc sed blandit libero volutpat.
     </p>
